2c45bd48a58bf687ea6a33b94675bd9d55da9d9a
Muhammed Jinas <57747927+JinasMuhammed@users.noreply.github.com>
2020-01-10 20:38:44 +0530

    Add files via upload

---
+++ build/precache-manifest.c708a0b5bdc880c46e04b009d61127b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b15ea54c2d5ff54e7345e70daa23002",
+    "url": "/index.html"
+  },
+  {
+    "revision": "03250c93e36c13190856",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "350e88b30ce398ecf77d",
+    "url": "/static/js/2.7948d624.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.7948d624.chunk.js.LICENSE"
+  },
+  {
+    "revision": "03250c93e36c13190856",
+    "url": "/static/js/main.f2abed8a.chunk.js"
+  },
+  {
+    "revision": "d496b52f21a84ebe3c01",
+    "url": "/static/js/runtime-main.8df57d0a.js"
+  }
+]);
